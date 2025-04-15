f91021193b4595a89f873dc219204857d285f5e4
ajeena <ajeena.sics@gmail.com>
2025-04-15 16:29:13 +0530

    register login integration

--- client/src/components/Landing/NavLanding.tsx
+++ client/src/components/Landing/NavLanding.tsx
@@ -1,39 +1,71 @@
-import React from 'react';
-import { Button, ButtonGroup, Navbar, Dropdown, DropdownButton } from "react-bootstrap";
-import './LandingPage.css';
-import { Link, Links } from 'react-router-dom';
+import React from "react";
+import {
+  Button,
+  ButtonGroup,
+  Navbar,
+  Dropdown,
+  DropdownButton,
+} from "react-bootstrap";
+import "./LandingPage.css";
+import { Link, Links } from "react-router-dom";
 
 function NavLanding() {
   return (
     <div>
       <Navbar className="navbar-custom py-4" expand="lg">
         <Link to="/">
-      <h1 className="text-2xl font-bold text-light ms-5 underline">ArtisanConnect</h1>
-</Link>
+          <h1 className="text-2xl font-bold text-light ms-5 text-decoration-none">
+            ArtisanConnect
+          </h1>
+        </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse className="justify-content-end">
           <div className="nav-buttons d-flex gap-2 align-items-center">
-            
             {/* Become a Seller Dropdown */}
-            <DropdownButton variant="outline-light" title="Become a Artisans" className="seller-btn">
-              <Dropdown.Item href=""><Link to="/artisan/login">Login</Link></Dropdown.Item>
-              <Dropdown.Item href=""><Link to="/artisan/register">Register</Link>  </Dropdown.Item>
+            <DropdownButton
+              variant="outline-light"
+              title="Become a Artisans"
+              className="seller-btn"
+            >
+              <Dropdown.Item href="">
+                <Link to="/artisan/login">Login</Link>
+              </Dropdown.Item>
+              <Dropdown.Item href="">
+                <Link to="/artisan/register">Register</Link>{" "}
+              </Dropdown.Item>
             </DropdownButton>
             <ButtonGroup>
               {/* Login Dropdown */}
-              <DropdownButton as={ButtonGroup} variant="outline-light" title="Login" className="login-btn">
-              <Dropdown.Item href=""><Link to="/buyer/login">Buyer</Link></Dropdown.Item>
-                <Dropdown.Item href=""><Link to="/deliveryagent/login">Delivery Agents</Link> </Dropdown.Item>
+              <DropdownButton
+                as={ButtonGroup}
+                variant="outline-light"
+                title="Login"
+                className="login-btn"
+              >
+                <Dropdown.Item href="">
+                  <Link to="/buyer/login">Buyer</Link>
+                </Dropdown.Item>
+                <Dropdown.Item href="">
+                  <Link to="/deliveryagent/login">Delivery Agents</Link>{" "}
+                </Dropdown.Item>
                 <Dropdown.Item href="">Administrator</Dropdown.Item>
               </DropdownButton>
 
               {/* Sign Up Dropdown */}
-              <DropdownButton as={ButtonGroup} variant="light" title="Sign Up" className="signup-btn">
-              <Dropdown.Item href=""><Link to="/buyer/register">Buyer</Link></Dropdown.Item>
-                <Dropdown.Item href=""><Link to="/deliveryagent/register">Delivery Agents</Link></Dropdown.Item>
+              <DropdownButton
+                as={ButtonGroup}
+                variant="light"
+                title="Sign Up"
+                className="signup-btn"
+              >
+                <Dropdown.Item href="">
+                  <Link to="/buyer/register">Buyer</Link>
+                </Dropdown.Item>
+                <Dropdown.Item href="">
+                  <Link to="/deliveryagent/register">Delivery Agents</Link>
+                </Dropdown.Item>
               </DropdownButton>
             </ButtonGroup>
-
           </div>
         </Navbar.Collapse>
       </Navbar>
@@ -42,4 +74,3 @@ function NavLanding() {
 }
 
 export default NavLanding;
-
